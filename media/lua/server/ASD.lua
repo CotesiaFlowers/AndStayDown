b631e1ab315aa87f14f61ff0d40ce5dd24577e5f
CotesiaFlowers <cotesiaflowers@proton.me>
2025-01-12 16:06:43 +0000

    implemented sandbox options

--- media/lua/server/ASD.lua
+++ media/lua/server/ASD.lua
@@ -1,20 +1,31 @@
-Clever = {}
-Squares = {}
+local gameTime
+
+local sandboxASD = SandboxVars.ASD
+
+Events.OnGameTimeLoaded.Add(function()
+    gameTime = GameTime.getInstance()
+end)
+
+local Clever = {}
+-- local Squares = {}
 
 local Reanimate = function(corpse)
-	local reanimate_time = getGameTime():getWorldAgeHours() + 0.5
+	local reanimate_time = gameTime:getWorldAgeHours() + (sandboxASD.ReanimateTimeMin + ZombRand(101) * (sandboxASD.ReanimateTimeMax - sandboxASD.ReanimateTimeMin)) / 60.0
+
+	if corpse:isSkeleton() and not sandboxASD.ReanimateSkeletons then return end
+	if corpse:isPlayer() and not sandboxASD.ReanimatePlayers then return end
 
 	local corpseModData = corpse:getModData()
 
 	if (corpseModData.dead ~= true) then
-		if 50 <= ZombRand(100) then
-			corpseModData.dead = true
-		else
-			if 2 > ZombRand(100) then
-				Clever[#Clever + 1] = { corpse, reanimate_time - 0.5 }
+		if sandboxASD.ReanimateChance > ZombRand(100) then
+			if sandboxASD.FakeDeadChance > ZombRand(100) then
+				Clever[#Clever + 1] = { corpse, reanimate_time }
 			else
 				corpse:setReanimateTime(reanimate_time)
 			end
+		else
+			corpseModData.dead = true
 		end
 	end
 end
@@ -35,18 +46,22 @@ local function LoadGridsquare(square)
 	end
 end
 
-Events.LoadGridsquare.Add(LoadGridsquare)
-
-local function OnZombieDead(zombie)
-	Squares[#Squares + 1] = zombie:getCurrentSquare()
+if sandboxASD.ReanimateNearbyCorpses then
+	Events.LoadGridsquare.Add(LoadGridsquare)
 end
 
-Events.OnZombieDead.Add(OnZombieDead)
+-- local function OnCharacterDeath(character)
+-- 	if isServer() then
+-- 		Squares[#Squares + 1] = character:getCurrentSquare()
+-- 	end
+-- end
+
+Events.OnCharacterDeath.Add(OnCharacterDeath)
 
 local function EveryTenMinutes()
-	for i=1, #Squares do
-		ReanimateSquare(table.remove(Squares))
-	end
+	-- for i=1, #Squares do
+	-- 	ReanimateSquare(table.remove(Squares))
+	-- end
 
 	for i=1, #Clever do
 		if Clever[#Clever][2] >= getGameTime():getWorldAgeHours() then -- reanimate_time >= now
@@ -61,14 +76,10 @@ end
 
 Events.EveryTenMinutes.Add(EveryTenMinutes)
 
--- local function GetSquareNearPlayer(player, args)
--- 	player:getCell():getGridSquare(args[1], args[2], args[3])
--- end
+local function OnClientCommand(module, command, player, args)
+	if (module == "ASD" and command == "ReanimateNearPlayer") then
+		ReanimateSquare(player:getCell():getGridSquare(args[1], args[2], args[3]))
+	end
+end
 
--- local function OnClientCommand(module, command, player, args)
--- 	if (module == "ASD" and command == "ReanimateNearPlayer") then
--- 		ReanimateSquare(GetSquareNearPlayer(player, args))
--- 	end
--- end
-
--- Events.OnClientCommand.Add(OnClientCommand)
+Events.OnClientCommand.Add(OnClientCommand)
--- media/lua/shared/ASD.lua
+++ media/lua/shared/ASD.lua
@@ -1,8 +1,8 @@
--- local function OnContainerUpdate(corpse)
--- 	if corpse and instanceof(corpse, "IsoDeadBody") then
--- 		local square = corpse:getCurrentSquare()
--- 		sendClientCommand("ASD", "ReanimateNearPlayer", { square:getX(), square:getY(), square:getZ() })
--- 	end
--- end
+local function OnContainerUpdate(corpse)
+	if corpse and instanceof(corpse, "IsoDeadBody") then
+		local square = corpse:getCurrentSquare()
+		sendClientCommand("ASD", "ReanimateNearPlayer", { square:getX(), square:getY(), square:getZ() })
+	end
+end
 
--- Events.OnContainerUpdate.Add(OnContainerUpdate)
+Events.OnContainerUpdate.Add(OnContainerUpdate)
